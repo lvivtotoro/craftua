6905580f78922fe10681689cd542426feae9a53a
Midnightas <pan.totoro@gmail.com>
2016-02-27 10:54:55 +0200

    Each tribe now has it's own advantages. Read README for details.

--- CraftUkr/src/org/midnightas/craftukr/CraftUkr.java
+++ CraftUkr/src/org/midnightas/craftukr/CraftUkr.java
@@ -2,6 +2,7 @@ package org.midnightas.craftukr;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,15 +26,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class CraftUkr extends JavaPlugin implements Listener {
@@ -104,24 +109,36 @@ public class CraftUkr extends JavaPlugin implements Listener {
 						return true;
 					}
 				}
-				Bukkit.broadcastMessage(id + "");
 				if (id <= 3 && id > 0) {
-					if (!instance.TRIBES[id - 1].members.contains(((Player) sender).getUniqueId())) {
-						instance.TRIBES[id - 1].members.add(((Player) sender).getUniqueId());
-						sendMessageWithHeader(sender,
-								new String[] { _c + "Вітаємо вас до племена:", _b + instance.TRIBES[id - 1].name });
-						if (!instance.config0.contains(((Player) sender).getUniqueId() + "")
-								|| !instance.config0.getStringList(((Player) sender).getUniqueId() + "")
-										.contains(instance.TRIBES[id - 1].id)) {
-							List<String> firstTimePlayers = instance.config0
-									.getStringList(((Player) sender).getUniqueId() + "");
-							firstTimePlayers.add(instance.TRIBES[id - 1].id + "");
-							instance.config0.set(((Player) sender).getUniqueId() + "", firstTimePlayers);
-							for (String s : instance.config1.getStringList(instance.TRIBES[id - 1].id + ".kit"))
-								((Player) sender).getInventory().addItem(deserializeItem(s));
+					if (getTribeFromPlayer(((Player) sender).getUniqueId()) == null) {
+						if (instance.warnedPlayers.contains(((Player) sender).getUniqueId())) {
+							instance.TRIBES[id - 1].members.add(((Player) sender).getUniqueId());
+							sendMessageWithHeader(sender,
+									new String[] { _c + "Вітаємо вас до племена:", _b + instance.TRIBES[id - 1].name });
+							instance.warnedPlayers.remove(((Player) sender).getUniqueId());
+							if (!instance.config0.contains(((Player) sender).getUniqueId() + "")
+									|| !instance.config0.getStringList(((Player) sender).getUniqueId() + "")
+											.contains(instance.TRIBES[id - 1].id)) {
+								List<String> firstTimePlayers = instance.config0
+										.getStringList(((Player) sender).getUniqueId() + "");
+								firstTimePlayers.add(instance.TRIBES[id - 1].id + "");
+								instance.config0.set(((Player) sender).getUniqueId() + "", firstTimePlayers);
+								for (String s : instance.config1.getStringList(instance.TRIBES[id - 1].id + ".kit"))
+									((Player) sender).getInventory().addItem(deserializeItem(s));
+								instance.giveEffects((Player) sender);
+							}
+						} else {
+							sendMessageWithHeader(sender,
+									new String[] {
+											"Ви точно хочете брати участь у племені " + instance.TRIBES[id - 1].name
+													+ "?",
+											"Ви не зможете залишити цього племена!",
+											"Напишітьте команду щерез якщо ви впевнині.",
+											"Цей поклик показує перемоги кожної плем'ї:", "ПОКЛИК ТУТ" });
+							instance.warnedPlayers.add(((Player) sender).getUniqueId());
 						}
 					} else {
-						sendMessageWithHeader(sender, new String[] { _c + "Ви вже є у цьому племені." });
+						sendMessageWithHeader(sender, new String[] { _c + "Ви вже є у племені!" });
 					}
 				} else {
 					sendMessageWithHeader(sender, "Є тільки 3 племена!");
@@ -156,12 +173,29 @@ public class CraftUkr extends JavaPlugin implements Listener {
 	public Tribe TRIBE_FIRE = new Tribe("Вогонь", 1);
 	public Tribe TRIBE_AIR = new Tribe("Повітря", 2);
 	public Tribe[] TRIBES = new Tribe[] { TRIBE_EARTH, TRIBE_FIRE, TRIBE_AIR };
+	public List<UUID> warnedPlayers = new ArrayList<UUID>();
+	public List<UUID> earthCooldown = new ArrayList<UUID>();
+
+	public BukkitRunnable damageFireUnderwater;
 
 	public void onEnable() {
 		instance = this;
 		config = getConfig();
 		initConfigs();
 		Bukkit.getPluginManager().registerEvents(this, this);
+		damageFireUnderwater = new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					if (TribesFunction.getTribeFromPlayer(p.getUniqueId()) == TRIBE_FIRE) {
+						if (p.getLocation().getBlock().getType() == Material.WATER) {
+							p.damage(1);
+						}
+					}
+				}
+			}
+		};
+		damageFireUnderwater.runTaskTimer(this, 20, 10);
 		getCommand("t").setExecutor(new TribesFunction());
 		getCommand("t").setAliases(Arrays.asList("tribes"));
 		getCommand("f").setExecutor(new FactionsFunction());
@@ -207,16 +241,106 @@ public class CraftUkr extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
-		Player player = event.getPlayer();
-		if (TribesFunction.getTribeFromPlayer(event.getPlayer().getUniqueId()) == TRIBE_AIR) {
+		giveEffects(event.getPlayer());
+	}
+
+	@EventHandler
+	public void onPlayerRespawnEvent(PlayerRespawnEvent event) {
+		giveEffects(event.getPlayer());
+	}
+
+	public void giveEffects(Player player) {
+		Tribe tribe = TribesFunction.getTribeFromPlayer(player.getUniqueId());
+		if (tribe == TRIBE_AIR) {
 			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 1));
 			player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+		} else if (tribe == TRIBE_FIRE) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, Integer.MAX_VALUE, 1));
+		} else if (tribe == TRIBE_EARTH) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, Integer.MAX_VALUE, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+		}
+	}
+
+	@EventHandler
+	public void onRightClickBlock(PlayerInteractEvent event) {
+		Tribe tribe = TribesFunction.getTribeFromPlayer(event.getPlayer().getUniqueId());
+		if (tribe == TRIBE_FIRE) {
+			if (!event.getPlayer().isSneaking() && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+				Material material = event.getClickedBlock().getType();
+				if (material == Material.IRON_ORE) {
+					event.getClickedBlock().setType(Material.AIR);
+					event.getClickedBlock().getWorld().dropItemNaturally(event.getClickedBlock().getLocation(),
+							new ItemStack(Material.IRON_INGOT, 1));
+				} else if (material == Material.GOLD_ORE) {
+					event.getClickedBlock().setType(Material.AIR);
+					event.getClickedBlock().getWorld().dropItemNaturally(event.getClickedBlock().getLocation(),
+							new ItemStack(Material.GOLD_INGOT, 1));
+				}
+			} else {
+				if (!day(event.getPlayer().getWorld()) && event.getAction() == Action.RIGHT_CLICK_BLOCK)
+					event.getClickedBlock().getRelative(BlockFace.UP).setType(Material.FIRE);
+			}
+		} else if (tribe == TRIBE_EARTH) {
+			if (event.getPlayer().isSneaking() && event.getAction() == Action.RIGHT_CLICK_BLOCK &&
+					!earthCooldown.contains(event.getPlayer().getUniqueId())) {
+				final Player player = event.getPlayer();
+				double startX = 0.0D;
+				double startY = 0.0D;
+				double startZ = 0.0D;
+				World world = player.getWorld();
+				Material material = Material.BEDROCK;
+				for (int x = 0; x < 3; x++) {
+					for (int z = 0; z < 3; z++) {
+						for (int y = 0; y <= 3; y++) {
+							Location loc = new Location(world, startX + x, startY + y, startZ + z);
+							if (y != 3 && y != 0) {
+								if ((x >= 0 && z == 0) || (x >= 0 && z == 2) || (x == 0 && z >= 0)
+										|| (x == 2 && z >= 0))
+									loc.getBlock().setType(material);
+							} else {
+								loc.getBlock().setType(material);
+							}
+						}
+					}
+				}
+				earthCooldown.add(player.getUniqueId());
+				new BukkitRunnable() {
+					public void run() {
+						double startX = 0.0D;
+						double startY = 0.0D;
+						double startZ = 0.0D;
+						World world = player.getWorld();
+						Material material = Material.AIR;
+						for (int x = 0; x < 3; x++) {
+							for (int z = 0; z < 3; z++) {
+								for (int y = 0; y <= 3; y++) {
+									Location loc = new Location(world, startX + x, startY + y, startZ + z);
+									if (y != 3 && y != 0) {
+										if ((x >= 0 && z == 0) || (x >= 0 && z == 2) || (x == 0 && z >= 0)
+												|| (x == 2 && z >= 0))
+											loc.getBlock().setType(material);
+									} else {
+										loc.getBlock().setType(material);
+									}
+								}
+							}
+						}
+						new BukkitRunnable() {
+							public void run() {
+								earthCooldown.remove(player.getUniqueId());
+							}
+						}.runTaskLater(CraftUkr.this, 24000);
+					}
+				}.runTaskLater(this, 20 * 60l);
+			}
 		}
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerMoveEvent(PlayerMoveEvent event) {
-		if (TribesFunction.getTribeFromPlayer(event.getPlayer().getUniqueId()) == TRIBE_AIR) {
+		Tribe tribe = TribesFunction.getTribeFromPlayer(event.getPlayer().getUniqueId());
+		if (tribe == TRIBE_AIR) {
 			Player player = event.getPlayer();
 			if (player.isFlying()) {
 				return;
@@ -226,6 +350,10 @@ public class CraftUkr extends JavaPlugin implements Listener {
 				Vector dir = player.getLocation().getDirection().multiply(0.25);
 				player.setVelocity(new Vector(dir.getX(), player.getVelocity().getY() * 0.5, dir.getZ()));
 			}
+		} else if (tribe == TRIBE_FIRE) {
+			if (event.getPlayer().getLocation().getBlock().getType() == Material.WATER) {
+				event.getPlayer().damage(1);
+			}
 		}
 	}
 
@@ -233,9 +361,14 @@ public class CraftUkr extends JavaPlugin implements Listener {
 	public void onFallDamageEvent(EntityDamageEvent event) {
 		if (event.getEntity() instanceof Player) {
 			Player player = (Player) event.getEntity();
-			if (TribesFunction.getTribeFromPlayer(player.getUniqueId()) == TRIBE_AIR) {
+			Tribe tribe = TribesFunction.getTribeFromPlayer(player.getUniqueId());
+			if (tribe == TRIBE_AIR) {
 				if (event.getCause() == DamageCause.FALL) {
 					event.setDamage(event.getDamage() * 0.1);
+				}
+			} else if (tribe == TRIBE_EARTH) {
+				if (event.getCause() == DamageCause.FALL) {
+					event.setDamage(event.getDamage() * 1.5);
 				}
 			}
 		}
@@ -268,6 +401,11 @@ public class CraftUkr extends JavaPlugin implements Listener {
 		String[] splittedString = s.split("\\|");
 		return new Location(Bukkit.getWorld(splittedString[3]), Integer.parseInt(splittedString[0]),
 				Integer.parseInt(splittedString[1]), Integer.parseInt(splittedString[2]));
+	}
+
+	public boolean day(World world) {
+		long time = world.getTime();
+		return time < 12300 || time > 23850;
 	}
 
 	@SafeVarargs
